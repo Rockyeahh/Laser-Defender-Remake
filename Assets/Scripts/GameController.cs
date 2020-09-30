8f883252dc2e07b9e5fbeb9bedb207df50d9f7ad
Charlie <chaz.com@hotmail.co.uk>
2020-09-30 17:23:13 +0100

    Enemy ships now enable and disable at the right time. The game controller script just needs a fully working Reset function beyond a print statement.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,23 +4,50 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject enemyPrefab;
+    public GameObject[] enemies;
+    public GameObject enemyBossPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        // Enable Enemy gameObjects/children.
+        // Enable Enemy gameObjects/children. // Game objects tagged as Enemy set disable
+        if (enemies == null)
+            enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject Enemy in enemies)
+        {
+            print("work you cunt");
+            Enemy.SetActive(true);
+            print("Set enemy active");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            Reset();
+        }
     }
 
     // Spawn Enemies // test by spawning one enemy first. Then if it works have them spawn as a whole formation under a prefab
+    void SpawnEnemies()
+    {
+        //GameObject enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity) as GameObject;
+    }
 
     // Spawn Boss
+    void SpawnBoss()
+    {
+        //GameObject enemyBoss = Instantiate(enemyBossPrefab, transform.position, Quaternion.identity) as GameObject;
+    }
 
-    // Reset Eneimes
-
+    // Reset Eneimes    // Reset Enemy transform.position, animations?
+    void Reset()
+    {
+        print("Reset Enemies");
+    }
 
 }
