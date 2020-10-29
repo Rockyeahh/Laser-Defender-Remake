4c4520ada210a4db110d55ed590c9203847d0295
Charlie <chaz.com@hotmail.co.uk>
2020-10-29 16:56:11 +0000

    added trigger on exit but still need to try OnCollisionStay

--- Assets/Scripts/EnemyChecker.cs
+++ Assets/Scripts/EnemyChecker.cs
@@ -10,10 +10,19 @@ public class EnemyChecker : MonoBehaviour
     // Maybe that get and set thing will work.
 
         // Update() { if it ain't triggering then it's true. }
+        // if enemyCheckerCollisionHappened { safeToShoot = false; else fafeToShoot = true; }
 
     void OnTriggerEnter2D(Collider2D collider)  // Does is Trigger need to be ticked in the Enemy Checker inspector.
     {
-        safeToShoot = false;
+        print("Safe to shoot = false");
+        safeToShoot = false;    // Safe to shoot = false is only being called when the players laser reaches the box 
+                                // but then it kills the enemy before it can start shooting.
+                                // I need something that constantly checks if it is there colliding, not just checks if it has entered or exited.
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        safeToShoot = true;
     }
 
 }
